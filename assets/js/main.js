6d070aa673c24ec79c31e5cf9c821bd2c955506f
ultimecreation <naserbechata@gmail.com>
2022-07-12 15:26:19 +0200

    formatted document + added JSdocs and comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,20 @@ window.addEventListener('DOMContentLoaded', async () => {
     const recipeContainer = document.querySelector('#recipe-container')
     const filterForms = document.querySelectorAll('#form-container form')
     const tagContainer = document.querySelector('#tag-container')
-    // const formBtns = filterForms.querySelector('button')
 
 
     // FUNCTIONS
+    /**
+     * create the html for each recipe and insert it in the dom
+     *
+     * @return  {void}  
+     */
     const displayRecipes = () => {
         let output = ''
 
         recipesData.map(recipe => {
+
+            // create string for ingredients list outside of the main output
             let recipeIngredientsList = ''
             recipe.ingredients.forEach(ingredient => {
                 recipeIngredientsList += `
@@ -21,6 +27,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                 `
             })
 
+            // generate main output
             output += `
                 <div class="card  col-lg-6 col-xl-4 mx-0 my-3  p-0 border-0 rounded-2">
                     <img src="https://via.placeholder.com/380" class="card-img-top img-fluid" alt="${recipe.name}">
@@ -41,14 +48,25 @@ window.addEventListener('DOMContentLoaded', async () => {
 
         })
 
+        // insert main out in the dom
         recipeContainer.innerHTML = output
     }
 
+    /**
+     * show/hide the filters dropdowns
+     *
+     * @param   {HTMLElement}  filterForm  
+     *
+     * @return  {void}              
+     */
     const handleClickOnFiltersBtn = () => filterForms.forEach(filterForm => {
+        // bind dom element
         const formBtn = filterForm.querySelector('button')
 
         formBtn.addEventListener('click', e => {
             e.preventDefault()
+
+            // bind variables
             const currentBtn = e.target.closest('button')
             const icon = currentBtn.querySelector('i')
             const ul = currentBtn.nextElementSibling
@@ -72,19 +90,31 @@ window.addEventListener('DOMContentLoaded', async () => {
         })
     })
  
+    /**
+     * add a new button into the tags container
+     * each time the user click on 1 of the dropdown links
+     *
+     * @param   {HTMLeLEMENT}  filterForm  
+     *
+     * @return  {VOID}              
+     */
     const handleClickOnFiltersLinks = () =>  filterForms.forEach(filterForm => {
+        // bind links from dom
         const links = filterForm.querySelectorAll('ul a.dropdown-item')
 
         links.forEach(link =>{
             link.addEventListener('click', e=>{
+                // bind dom elements and initialize variables 
                 const parentElement = e.target.parentElement.parentElement
                 const linkTextContent = e.target.textContent
                 let output = ''
                 let classType = 'warning'
 
+                // define a classType for each type of dropdowns
                 if(parentElement.id.startsWith('ingredients')) classType = 'primary'
                 else if(parentElement.id.startsWith('appliance')) classType = 'success'
                 
+                // generate the html button
                 output = `
                     <button class="btn btn-small btn-${classType} m-1">
                         ${linkTextContent}
@@ -93,27 +123,42 @@ window.addEventListener('DOMContentLoaded', async () => {
                     </button>
                 `
 
+                // insert the button element into the tags container
                 tagContainer.innerHTML += output
-                console.log(e.target, parentElement)
             })
         })
         
     })
 
+    /**
+     * remove a button from tags container
+     * when the user click on it
+     *
+     * @return  {void}  
+     */
     const handleTagDeletionFromTagContainer = ()=>{
         tagContainer.addEventListener('click', e => {
+
+            // bind the closest button and remove it from the dom
             const itemToRemove = e.target.closest('button')
             itemToRemove.remove()
-            console.log(itemToRemove)
         })
     }
    
+    /**
+     * revert the form and dropdown to its initial state
+     *
+     * @return  {void}  
+     */
     const resetFiltersDisplay = () => {
         filterForms.forEach(filterForm => {
+
+            // bind dom elements
             const inputGroup = filterForm.querySelector('.input-group')
             const ul = filterForm.querySelector('ul')
             const icon =  filterForm.querySelector('i')
 
+            // apply the changes on the form and dropdown content
             inputGroup.style.width = '160px'
             filterForm.style.width = '160px'
             ul.style.display = 'none'
@@ -121,22 +166,40 @@ window.addEventListener('DOMContentLoaded', async () => {
         })
     }
 
+    /**
+     * toggle the arrow icon up/down 
+     *
+     * @param   {HTMLElement}  btn 
+     *
+     * @return  {void}       
+     */
     const toggleIcon = (btn) => {
+        // bind dom element
         const icon = btn.querySelector('i')
 
+        // toggle the down/up arrow on the dropdown
         icon.classList.contains('fa-chevron-down')
             ? icon.classList.replace('fa-chevron-down','fa-chevron-up')
             : icon.classList.replace('fa-chevron-up','fa-chevron-down')
     }
 
+    /**
+     * generate workable/flattered arrays of tags to be used to populate the dropdowns
+     *
+     * @return  {[object]}  
+     */
     const getRecipeTags = () => {
+        // create variables to be filled later
         let ingredients = []
         let appliance = []
         let ustensils = []
 
+    
+        // loop through the filters forms to know what kind of data to look for
         filterForms.forEach(filterForm => {
             const itemsToFetch = filterForm.querySelector('button').dataset.items
 
+            // loop through the stored data to fill workable arrays with unique values inside
             recipesData.forEach((recipe) => {
                 if (itemsToFetch === 'ingredients') {
                     recipe[itemsToFetch].forEach(item => {
@@ -164,32 +227,42 @@ window.addEventListener('DOMContentLoaded', async () => {
         ]
     }
 
-    const populateFilters = () => {
-        const tags = [...getRecipeTags()]
-        const ingredients = tags[0].ingredients
-        const appliance = tags[1].appliance
-        const ustensils = tags[2].ustensils
-
-        document.querySelector('#ingredients-container').innerHTML = generateStringOutput(ingredients)
-        document.querySelector('#appliance-container').innerHTML = generateStringOutput(appliance)
-        document.querySelector('#ustensils-container').innerHTML = generateStringOutput(ustensils)
-    }
-
-    const generateStringOutput = items =>{
+    /**
+     * generate the html markup to be inserted in the dom
+     *
+     * @var {string}
+     */
+    const generateStringOutput = items => {
         let output = ''
-        items.forEach(item=> {
+        items.forEach(item => {
             output += `<li class=""><a class="dropdown-item text-white" href="#">${item}</a></li> `
         })
         return output
     }
 
+    /**
+     * populate the multiple dropdowns with previous fromatted html
+     *
+     * @return  {void} 
+     */
+    const populateFilters = () => {
+        // extract/bind data to populate the dropdowns
+        const tags = [...getRecipeTags()]
+        const ingredients = tags[0].ingredients
+        const appliance = tags[1].appliance
+        const ustensils = tags[2].ustensils
+
+        // insert dropdowns content in the dom
+        document.querySelector('#ingredients-container').innerHTML = generateStringOutput(ingredients)
+        document.querySelector('#appliance-container').innerHTML = generateStringOutput(appliance)
+        document.querySelector('#ustensils-container').innerHTML = generateStringOutput(ustensils)
+    }
+
+    // INITIALIZATION
     displayRecipes()
     populateFilters()
     handleClickOnFiltersBtn()
     handleClickOnFiltersLinks()
     handleTagDeletionFromTagContainer()
-    // EVENT LISTENERS
-
-
 
 })
