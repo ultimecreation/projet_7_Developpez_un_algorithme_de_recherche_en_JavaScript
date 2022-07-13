284b7166aee0ba196968a3108d995228cdca04da
ultimecreation <naserbechata@gmail.com>
2022-07-13 13:56:54 +0200

    added serach algorithm

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,27 +76,27 @@ window.addEventListener('DOMContentLoaded', async () => {
             const ulStyle = window.getComputedStyle(ul);
             const input = filterForm.querySelector('input')
             const inputPlaceHolderString = icon.classList.contains('fa-chevron-up')
-                                            ? `${currentBtn.dataset.name.charAt(0).toUpperCase()}${currentBtn.dataset.name.slice(1)}`
-                                            : `Rechercher un ${currentBtn.dataset.name.slice(0,-1)}`
- 
+                ? `${currentBtn.dataset.name.charAt(0).toUpperCase()}${currentBtn.dataset.name.slice(1)}`
+                : `Rechercher un ${currentBtn.dataset.name.slice(0, -1)}`
+
             // dropdown is already open,the user clicked to close it
-            if(icon.classList.contains('fa-chevron-up')){
+            if (icon.classList.contains('fa-chevron-up')) {
                 resetFiltersDisplay()
-                icon.classList.replace('fa-chevron-up','fa-chevron-down')
-                input.setAttribute('placeholder',inputPlaceHolderString)
+                icon.classList.replace('fa-chevron-up', 'fa-chevron-down')
+                input.setAttribute('placeholder', inputPlaceHolderString)
                 return
             }
-            
+
             // the dropdown is closed, open it
             resetFiltersDisplay()
             toggleIcon(currentBtn)
             ul.style.display = 'block'
             parent.style.width = ulStyle.width
             inputGroup.style.width = ulStyle.width
-            input.setAttribute('placeholder',inputPlaceHolderString)
+            input.setAttribute('placeholder', inputPlaceHolderString)
         })
     })
- 
+
     /**
      * add a new button into the tags container
      * each time the user click on 1 of the dropdown links
@@ -105,12 +105,12 @@ window.addEventListener('DOMContentLoaded', async () => {
      *
      * @return  {VOID}              
      */
-    const handleClickOnFiltersLinks = () =>  filterForms.forEach(filterForm => {
+    const handleClickOnFiltersLinks = () => filterForms.forEach(filterForm => {
         // bind links from dom
         const links = filterForm.querySelectorAll('ul a.dropdown-item')
 
-        links.forEach(link =>{
-            link.addEventListener('click', e=>{
+        links.forEach(link => {
+            link.addEventListener('click', e => {
                 // bind dom elements and initialize variables 
                 const parentElement = e.target.parentElement.parentElement
                 const linkTextContent = e.target.textContent
@@ -118,9 +118,9 @@ window.addEventListener('DOMContentLoaded', async () => {
                 let classType = 'warning'
 
                 // define a classType for each type of dropdowns
-                if(parentElement.id.startsWith('ingredients')) classType = 'primary'
-                else if(parentElement.id.startsWith('appliance')) classType = 'success'
-                
+                if (parentElement.id.startsWith('ingredients')) classType = 'primary'
+                else if (parentElement.id.startsWith('appliance')) classType = 'success'
+
                 // generate the html button
                 output = `
                     <button class="btn btn-small btn-${classType} m-1 text-white">
@@ -134,7 +134,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                 tagContainer.innerHTML += output
             })
         })
-        
+
     })
 
     /**
@@ -143,7 +143,7 @@ window.addEventListener('DOMContentLoaded', async () => {
      *
      * @return  {void}  
      */
-    const handleTagDeletionFromTagContainer = ()=>{
+    const handleTagDeletionFromTagContainer = () => {
         tagContainer.addEventListener('click', e => {
 
             // bind the closest button and remove it from the dom
@@ -151,7 +151,7 @@ window.addEventListener('DOMContentLoaded', async () => {
             itemToRemove.remove()
         })
     }
-   
+
     /**
      * revert the form and dropdown to its initial state
      *
@@ -163,17 +163,17 @@ window.addEventListener('DOMContentLoaded', async () => {
             // bind dom elements
             const inputGroup = filterForm.querySelector('.input-group')
             const ul = filterForm.querySelector('ul')
-            const icon =  filterForm.querySelector('i')
-            const currentBtn =  filterForm.querySelector('button')
+            const icon = filterForm.querySelector('i')
+            const currentBtn = filterForm.querySelector('button')
             const input = filterForm.querySelector('input')
             const inputPlaceHolderString = `${currentBtn.dataset.name.charAt(0).toUpperCase()}${currentBtn.dataset.name.slice(1)}`
-                                            
+
             // apply the changes on the form and dropdown content
             inputGroup.style.width = '160px'
             filterForm.style.width = '160px'
             ul.style.display = 'none'
-            icon.classList.replace('fa-chevron-up','fa-chevron-down')
-            input.setAttribute('placeholder',inputPlaceHolderString)
+            icon.classList.replace('fa-chevron-up', 'fa-chevron-down')
+            input.setAttribute('placeholder', inputPlaceHolderString)
         })
     }
 
@@ -190,8 +190,8 @@ window.addEventListener('DOMContentLoaded', async () => {
 
         // toggle the down/up arrow on the dropdown
         icon.classList.contains('fa-chevron-down')
-            ? icon.classList.replace('fa-chevron-down','fa-chevron-up')
-            : icon.classList.replace('fa-chevron-up','fa-chevron-down')
+            ? icon.classList.replace('fa-chevron-down', 'fa-chevron-up')
+            : icon.classList.replace('fa-chevron-up', 'fa-chevron-down')
     }
 
     /**
@@ -205,7 +205,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         let appliance = []
         let ustensils = []
 
-    
+
         // loop through the filters forms to know what kind of data to look for
         filterForms.forEach(filterForm => {
             const itemsToFetch = filterForm.querySelector('button').dataset.items
@@ -245,6 +245,11 @@ window.addEventListener('DOMContentLoaded', async () => {
      */
     const generateStringOutput = items => {
         let output = ''
+
+        // items, return "no data" message
+        if(items.length === 0) return output = `<li class=""><a class="dropdown-item text-white" href="#">Pas de données</a></li> `
+
+        // some items received, return data
         items.forEach(item => {
             output += `<li class=""><a class="dropdown-item text-white" href="#">${item}</a></li> `
         })
@@ -258,11 +263,29 @@ window.addEventListener('DOMContentLoaded', async () => {
      */
     const populateFilters = (incomingRecipeData) => {
         // extract/bind data to populate the dropdowns
+        const ingredientsContainer = document.querySelector('#ingredients-container')
+        const applianceContainer = document.querySelector('#appliance-container')
+        const ustensilsContainer = document.querySelector('#ustensils-container')
         const tags = [...getRecipeTags(incomingRecipeData)]
         const ingredients = tags[0].ingredients
         const appliance = tags[1].appliance
         const ustensils = tags[2].ustensils
 
+        // generate splitted ul in x or y columns
+        if(ingredients.length < 10) ingredientsContainer.style.columns = 1
+        else if(ingredients.length < 20 ) ingredientsContainer.style.columns = 2
+        else ingredientsContainer.style.columns = 3
+        
+        // generate splitted ul in x or y columns
+        if(appliance.length < 10) applianceContainer.style.columns = 1
+        else if(appliance.length < 20 ) applianceContainer.style.columns = 2
+        else applianceContainer.style.columns = 3
+        
+        // generate splitted ul in x or y columns
+        if(ustensils.length < 10) ustensilsContainer.style.columns = 1
+        else if(ustensils.length < 20 ) ustensilsContainer.style.columns = 2
+        else ustensilsContainer.style.columns = 3
+        
         // insert dropdowns content in the dom
         document.querySelector('#ingredients-container').innerHTML = generateStringOutput(ingredients)
         document.querySelector('#appliance-container').innerHTML = generateStringOutput(appliance)
@@ -279,13 +302,66 @@ window.addEventListener('DOMContentLoaded', async () => {
     // EVENT LISTENERS
     mainRecipeSearchInput.addEventListener('keyup', e => {
         // bind data
-        const searchString = e.target.value 
+        const searchString = e.target.value.toLowerCase().trim()
 
         // ignore less than 3 characters search strings
-        if(searchString.length < 3) return 
+        if (searchString.length > 2) {
 
-        
-        console.log(e.target.value)
+            // initialize arrays to work with
+            let filteredRecipeIds = []
+            let filteredRecipes = []
+
+            // get matches from ingredients arrays
+            const RecipeIdsFromIngredientMatches = recipesData.reduce((accumulator, currentRecipe) => {
+
+                const { ingredients } = currentRecipe
+                ingredients.forEach(current => {
+                    // bind data
+                    let currentIngredient = current.ingredient.toLowerCase()
+                    let triggerReached = currentIngredient.indexOf(searchString) !== -1 
+                                        && !accumulator.includes(currentRecipe.id)
+                                         
+                    // match found on ingredients and recipe does not belong to accumulator, add it to accumulator
+                    if (triggerReached) accumulator.push(currentRecipe.id)
+                })
+                return accumulator
+            }, [])
+
+            const RecipeIdsFromTitleOrDescriptionMatches = recipesData.reduce((accumulator, currentRecipe) => {
+                // bind data
+                let currentTitle = currentRecipe.name.toLowerCase()
+                let currentDescription = currentRecipe.description.toLowerCase()
+                let triggerReached = ( currentTitle.indexOf(searchString) !== -1 || currentDescription.indexOf(searchString) !== -1 )
+                                    && !accumulator.includes(currentRecipe.id)
+
+                // match found on title/description and recipe does not belong to accumulator, add it to accumulator
+                if (triggerReached) accumulator.push(currentRecipe.id)
+                return accumulator
+            }, [])
+
+            // concat arrays and remove duplicate entries with new Set
+            filteredRecipeIds = Array.from(new Set([...RecipeIdsFromIngredientMatches,...RecipeIdsFromTitleOrDescriptionMatches]))
+            
+            // no record found, display appropriate message
+            if (filteredRecipeIds.length === 0) {
+                populateFilters([])
+                recipeContainer.innerHTML = `
+                    <div class="w-100 text-center">
+                        <i class="fa-solid fa-triangle-exclamation fa-5x text-danger"></i>
+                        <p class="text-center h2 w-75 mx-auto">Aucune recette ne correspond à votre critère "${searchString}".<br>
+                        Vous pouvez chercher "tarte aux pommes", "poisson"</p>
+                    </div>
+                    
+                `
+                return
+            }
+
+            // some records found, get and display them
+            filteredRecipes = recipesData.filter(recipe =>  filteredRecipeIds.includes(recipe.id))
+            displayRecipes(filteredRecipes)
+            populateFilters(filteredRecipes)
+            handleClickOnFiltersLinks()
+        }
     })
 
 })
